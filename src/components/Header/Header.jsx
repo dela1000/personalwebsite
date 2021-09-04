9ca522f0c5a24107d97fd8a55115c1118432965f
Daniel De La Rosa <dela1000@gmail.com>
2021-09-04 16:40:48 +0200

    add background to header dynamically

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react';
 import Navbar from '../Navbar/Navbar';
 import './Header.css';
+import { ThemeContext } from '../../contexts/theme';
 
 export default function Header() {
+  const [{ themeName }] = useContext(ThemeContext);
   return (
-    <div className="w-screen filter drop-shadow-sm items-center py-7 px-5 flex justify-around sticky top-0">
+    <header
+      id="top"
+      className={`${themeName}-header w-screen filter items-center py-7 px-5 flex justify-around sticky top-0`}
+    >
       <div className="nav-circle">
         <div className="pt-1.5 garamond text-3xl italic">D</div>
       </div>
       <Navbar />
-    </div>
+    </header>
   );
 }
