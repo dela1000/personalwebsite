ccb0e1e152e3d21c4a78e38e0880217c0354feb9
daniel <dela1000@gmail.com>
2021-09-04 10:54:57 +0200

    add eslintrc prettierrc and update files accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-
-import {BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
