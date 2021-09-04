215e845c6308b54d8b695cf595dc3c410eab0ac1
daniel <dela1000@gmail.com>
2021-09-04 11:58:26 +0200

    add material ui icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import { useContext } from 'react';
+import Brightness2Icon from '@material-ui/icons/Brightness2';
+import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded';
 import { ThemeContext } from '../../contexts/theme';
 import './Header.css';
 
 export default function Header() {
   const [{ themeName, toggleTheme }] = useContext(ThemeContext);
   return (
-    <div>
+    <div className="w-screen filter drop-shadow-sm items-center py-5 px-5 flex justify-around">
       <h3>HEADER</h3>
       <button
         type="button"
@@ -13,7 +15,7 @@ export default function Header() {
         className="center btn btn--icon"
         aria-label="toggle theme"
       >
-        {themeName === 'dark' ? <div>WbSunnyRoundedIcon</div> : <div>Brightness2Icon</div>}
+        {themeName === 'dark' ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
       </button>
     </div>
   );
