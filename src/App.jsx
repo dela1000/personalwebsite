dc7e12d3ef61b0006982e024336766d5e9fc1026
daniel <dela1000@gmail.com>
2021-09-04 11:20:17 +0200

    add initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './pages/Home/index';
+import Projects from './pages/Projects/index';
+import './tailwind.css';
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </Router>
+    <div id="container" className="flex flex-col h-screen">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
-
-export default App;
