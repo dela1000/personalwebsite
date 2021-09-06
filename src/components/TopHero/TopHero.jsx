757b83ea930bc2c502504cd2977b483920f08772
Daniel De La Rosa <dela1000@gmail.com>
2021-09-06 14:20:46 +0200

    add fonts, add primaryText and secondaryText to TopHero

--- src/components/TopHero/TopHero.jsx
+++ src/components/TopHero/TopHero.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 
-export default function TopHero({ imageLink }) {
+export default function TopHero({ imageLink, primaryText, secondaryText }) {
   return (
     <div className="relative w-full">
-      <div className="absolute w-screen h-auto text-center top-1/2">
-        <h2>TopHero</h2>
-        <b> Goes Here</b>
+      <div className="absolute center w-full" style={{ height: '100%' }}>
+        <div className="w-full text-center">
+          <div className="white--font text-7xl monserrat my-5">{primaryText}</div>
+          <div className="white--font garamond text-3xl my-5">{secondaryText}</div>
+        </div>
       </div>
       <img
         style={{ maxHeight: '700px' }}
@@ -19,4 +21,6 @@ export default function TopHero({ imageLink }) {
 
 TopHero.propTypes = {
   imageLink: PropTypes.string.isRequired,
+  primaryText: PropTypes.string.isRequired,
+  secondaryText: PropTypes.string.isRequired,
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,9 +6,25 @@ import largeImagesData from '../../adapters/imagesData';
 
 export default function Home({ windowType }) {
   return (
-    <div>
-      <TopHero imageLink={largeImagesData[windowType].merzougabw} />
-      <BottomHero imageLink={largeImagesData[windowType].halongbaybw} />
+    <div data-testid="Home" className="flex flex-col h-full">
+      <div className="flex-1 block">
+        <div className="mb-auto mt-auto">
+          <div className="flex flex-col">
+            <div className="flex justify-center">
+              <TopHero
+                imageLink={largeImagesData[windowType].merzougabw}
+                primaryText="DESIGN. DEVELOP. DELIVER."
+                secondaryText="Creating meaningful experiences through unique online ventures"
+              />
+            </div>
+            <div className="flex mb-auto justify-center">
+              <BottomHero imageLink={largeImagesData[windowType].halongbaybw} />
+            </div>
+
+            <div className="flex mb-auto justify-center">BUTTON GOES HERE</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
