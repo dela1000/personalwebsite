965ab323ec4ee4bb2087c8082414fc47009b597f
Daniel De La Rosa <dela1000@gmail.com>
2021-09-07 20:51:40 +0200

    tiny updates

--- src/components/TextSection/TextSection.jsx
+++ src/components/TextSection/TextSection.jsx
@@ -4,7 +4,7 @@ export default function TextSection({ name, text, description, developed }) {
   return (
     <div className="w-full text-center px-10 py-8">
       {name && <div className="header-text-size monserrat mb-2">{name}</div>}
-      <div className="header-text-size garamond mb-1">{text}</div>
+      <div className="sub-header-text-size garamond mb-5">{text}</div>
       <div className="description-text-size monserrat">{description}</div>
       {developed && <div className="description-text-size monserrat mt-10">{developed}</div>}
     </div>
