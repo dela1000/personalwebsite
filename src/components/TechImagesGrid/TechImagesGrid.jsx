23dfb2f2dfe4a371e20a3802e9d7365d1203f0d0
Daniel De La Rosa <dela1000@gmail.com>
2022-07-25 12:56:41 +0200

    fix positioning for vue

--- src/components/TechImagesGrid/TechImagesGrid.jsx
+++ src/components/TechImagesGrid/TechImagesGrid.jsx
@@ -6,8 +6,8 @@ export default function TechImagesGrid({ imagesData }) {
       {imagesData.map((imageData) => (
         <div
           key={imageData.name}
-          className={`mb-4 px-8 w-1/2 md:w-1/3 lg:w-1/6 image-transition-wrap group ${
-            imageData.name === 'Vue' && 'pt-5'
+          className={`mb-2 px-8 w-1/2 md:w-1/3 lg:w-1/6 image-transition-wrap group ${
+            imageData.name === 'Vue' && 'pt-4'
           }`}
         >
           <img
@@ -15,7 +15,11 @@ export default function TechImagesGrid({ imagesData }) {
             src={imageData.img}
             alt={imageData.name}
           />
-          <div className="flex pt-2 justify-center items-center">
+          <div
+            className={`flex pt-2 justify-center items-center ${
+              imageData.name === 'Vue' && 'mt-2'
+            }`}
+          >
             <div className="footer-text-size monserrat font-semibold opacity-0 group-hover:opacity-100 duration-300">
               {imageData.name}
             </div>
