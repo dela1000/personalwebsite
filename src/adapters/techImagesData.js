fe8091eece82a81db530b60a44782260f6279933
Daniel De La Rosa <dela1000@gmail.com>
2022-07-25 12:27:48 +0200

    add tech

--- src/adapters/techImagesData.js
+++ src/adapters/techImagesData.js
@@ -1,29 +1,33 @@
-import adobe from '../assets/logos/adobe.png';
-import angular from '../assets/logos/angular.png';
-import aws from '../assets/logos/aws.png';
-import bootstrap from '../assets/logos/bootstrap.png';
-import html from '../assets/logos/html.png';
-import css from '../assets/logos/css.png';
-import react from '../assets/logos/react.png';
-import node from '../assets/logos/node.png';
-import git from '../assets/logos/git.png';
-import ionic from '../assets/logos/ionic.png';
-import flutter from '../assets/logos/flutter.png';
-import grunt from '../assets/logos/grunt.png';
+import adobe from 'assets/logos/adobe.png';
+import angular from 'assets/logos/angular.png';
+import aws from 'assets/logos/aws.png';
+import bootstrap from 'assets/logos/bootstrap.png';
+import html from 'assets/logos/html.png';
+import css from 'assets/logos/css.png';
+import react from 'assets/logos/react.png';
+import node from 'assets/logos/node.png';
+import git from 'assets/logos/git.png';
+import ionic from 'assets/logos/ionic.png';
+import flutter from 'assets/logos/flutter.png';
+import grunt from 'assets/logos/grunt.png';
+import vue from 'assets/logos/vue.png';
+import tailwind from 'assets/logos/tailwind.png';
 
 const techImagesData = [
   { img: react, name: 'React' },
+  { img: vue, name: 'Vue' },
+  { img: angular, name: 'Angular' },
   { img: node, name: 'Node' },
-  { img: aws, name: 'AWS' },
-  { img: git, name: 'git' },
   { img: html, name: 'HTML' },
   { img: css, name: 'CSS' },
-  { img: angular, name: 'Angular' },
-  { img: adobe, name: 'Adobe' },
+  { img: git, name: 'git' },
+  { img: tailwind, name: 'TailwindCSS' },
+  { img: bootstrap, name: 'Bootstrap' },
   { img: ionic, name: 'Ionic' },
   { img: flutter, name: 'Flutter' },
   { img: grunt, name: 'Grunt' },
-  { img: bootstrap, name: 'Bootstrap' },
+  { img: aws, name: 'AWS' },
+  { img: adobe, name: 'Adobe' },
 ];
 
 export default techImagesData;
--- src/components/TechImagesGrid/TechImagesGrid.jsx
+++ src/components/TechImagesGrid/TechImagesGrid.jsx
@@ -2,17 +2,24 @@ import PropTypes from 'prop-types';
 
 export default function TechImagesGrid({ imagesData }) {
   return (
-    <div className="flex flex-wrap my-8 -mx-8">
+    <div className="flex flex-wrap my-8 -mx-8 justify-center">
       {imagesData.map((imageData) => (
         <div
           key={imageData.name}
-          className="mb-4 px-8 w-1/2 md:w-1/3 lg:w-1/6 image-transition-wrap"
+          className={`mb-4 px-8 w-1/2 md:w-1/3 lg:w-1/6 image-transition-wrap group ${
+            imageData.name === 'Vue' && 'pt-5'
+          }`}
         >
           <img
-            className="block h-auto w-full sepia mt-4 image-transition"
+            className="block h-auto w-full sepia mt-4 image-transition duration-500"
             src={imageData.img}
             alt={imageData.name}
           />
+          <div className="flex pt-2 justify-center items-center">
+            <div className="footer-text-size monserrat font-semibold opacity-0 group-hover:opacity-100 duration-300">
+              {imageData.name}
+            </div>
+          </div>
         </div>
       ))}
     </div>
