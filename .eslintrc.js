a5afcb38f0c55640009ed09d13de967d16d692e8
Daniel De La Rosa <dela1000@gmail.com>
2023-08-16 14:15:54 -0500

    fix parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,12 @@ module.exports = {
     jest: true,
   },
   extends: ['plugin:react/recommended', 'prettier'],
-  parser: '@babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
     ecmaFeatures: {
       jsx: true,
       experimentalObjectRestSpread: true,
